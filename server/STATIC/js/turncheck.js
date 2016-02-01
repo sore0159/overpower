553bb5fd30099b2fc6ced996d099fd0f9ba3504c
The Mule <3.7mules@gmail.com>
2016-02-01 04:28:16 -0600

    turn update blink javascript added

--- server/STATIC/js/turncheck.js
+++ server/STATIC/js/turncheck.js
@@ -1,11 +1,85 @@
-var curTurn = -1;
+console.log("CUR GAME IS", curGame);
+console.log("CUR TURN IS", curTurn);
 
-function getTurn() {
-    var url = "http://1";
-    var req = new XMLHttpRequest();
-    //req.addEventListener();
-    req.open("GET", url, true);
-    req.send();
-    var turn = 1;
-    return turn;
+(function() {
+var newTurn = false;
+var blink = false;
+var icon = document.querySelector('link[rel="shortcut icon"]');
+
+function log(msg) {
+    console.log("Overpower: "+msg);
 }
+
+function getTurn(cb) {
+    var req = new XMLHttpRequest();
+    function handleTurn() {
+        var err, turn;
+        if (req.status == 200) {
+            resp = JSON.parse(req.responseText);
+            if (resp.status == "success") {
+                turn = resp.data.turn;
+            } else if (resp.status == "fail") {
+                err = "json fail response from server:"+'\n'+resp.data;
+            } else if (resp.status == "error") {
+                err = "json error response from server:"+'\n'+resp.message;
+            } else {
+                err = "unknown json response from server";
+            }
+        } else {
+            err = req.status +'\n'+req.statusText;
+        }
+        cb(err, turn);
+    }
+    req.addEventListener("load", handleTurn);
+    req.addEventListener("error", function() {
+        cb("an error occured with the request");
+    });
+    req.open("GET", "/overpower/json/games/"+curGame, true);
+    req.send();
+}
+
+function turnCheck(isNew) {
+    if (newTurn) {
+        return isNew(true);
+    }
+    function gotTurn(err, turn) {
+        if (err) {
+            log("error fetching turn:", err);
+            return isNew(false);
+        }
+        if (turn > curTurn) {
+            newTurn = true;
+        }
+        return isNew(turn > curTurn);
+    }
+    getTurn(gotTurn);
+}
+
+function whenNew(newCheck) {
+    if (newCheck) {
+        var blocker = document.querySelector("div.blocker");
+        blocker.style.display = 'block';
+        blinkIco();
+    } else {
+        window.setTimeout(cycle, 15000);
+    }
+}
+
+function blinkIco() {
+    if (blink) {
+        icon.href = "/static/img/yd32.ico";
+    } else {
+        icon.href = "/static/img/yd32blink.ico";
+    }
+    blink = !blink;
+    window.setTimeout(blinkIco, 1000);
+}
+
+function cycle() {
+    turnCheck(whenNew);
+}
+
+cycle();
+})();
+
+
